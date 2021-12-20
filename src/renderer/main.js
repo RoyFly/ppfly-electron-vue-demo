b8fdcae8f4749be1a511e548b758a24f570cd9ab
public-007 <chaiyongqiang@iufc.cn>
2021-12-20 19:52:07 +0800

    update

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,10 +4,11 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+
 import VueSocketIO  from 'vue-socket.io'
 
 //引入socket.io配置连接
-Vue.use(new VueSocketIO ({
+Vue.use(new VueSocketIO({
     debug: true,
     connection: 'http://127.0.0.1:9092',
     vuex: {
@@ -16,14 +17,19 @@ Vue.use(new VueSocketIO ({
         mutationPrefix: 'SOCKET_'
     },
     // options: { path: "/my-app/" } //Optional options
-}))
+}));
 
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-Vue.http = Vue.prototype.$http = axios
-Vue.config.productionTip = false
-// 解决Electron Security Warning
-process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+//是否web程序
+if (!process.env.IS_WEB){
+    Vue.use(require('vue-electron'))
+}
+//axios
+Vue.http = Vue.prototype.$http = axios;
+//阻止显示生产模式的消息
+Vue.config.productionTip = false;
+//解决Electron Security Warning
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
 /* eslint-disable no-new */
 new Vue({
